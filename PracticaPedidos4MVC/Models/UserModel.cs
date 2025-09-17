20a46d85721369b65c2a02c4195d51df30308e3f
adrianlaymepresi <ignacioadrianlaymedelgado@gmail.com>
2025-09-16 20:32:49 -0400

    AdrianLayme/5) Crear modelo User -> Definir entidad con nombre, email, password, rol

---
+++ PracticaPedidos4MVC/Models/UserModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PracticaPedidos4MVC.Models
+{
+    public class User
+    {
+        [Display(Name = "Identificador Cliente")]
+        public int Id { get; set; }
+
+        [Display(Name = "Nombre de Usuario")]
+        public string Nombre { get; set; }
+
+        [Display(Name = "Correo electrónico del Usuario")]
+        public string Email { get; set; }
+
+        [Display(Name = "Contraseña de Usuario")]
+        public string Password { get; set; }
+
+        [Display(Name = "Rol de Usuario")]
+        public string Rol { get; set; }
+    }
+}
+
