3a36cf082e48910d2b78fd996a178ef8db219101
adrianlaymepresi <ignacioadrianlaymedelgado@gmail.com>
2025-09-16 20:44:26 -0400

    AdrianLayme/8) Crear modelo OrderItem -> Producto, cantidad, subtotal

---
+++ PracticaPedidos4MVC/Models/OrderItemModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PracticaPedidos4MVC.Models
+{
+    public class OrderItemModel
+    {
+        [Display(Name = "Identificador Ítem de Pedido")]
+        public int Id { get; set; }
+
+        [Display(Name = "Producto")]
+        public int IdProducto { get; set; }
+
+        [Display(Name = "Cantidad")]
+        public int Cantidad { get; set; }
+
+        [Display(Name = "Subtotal")]
+        public decimal Subtotal { get; set; }
+    }
+}
