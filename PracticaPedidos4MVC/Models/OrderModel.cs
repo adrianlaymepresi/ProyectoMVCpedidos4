6c1388fae7006d0dbd8cd46c9e01f9fb7d9aa82b
adrianlaymepresi <ignacioadrianlaymedelgado@gmail.com>
2025-09-16 20:40:36 -0400

    AdrianLayme/7) Crear modelo Order -> Cliente, fecha, estado, total

---
+++ PracticaPedidos4MVC/Models/OrderModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PracticaPedidos4MVC.Models
+{
+    public class OrderModel
+    {
+        [Display(Name = "Identificador Pedido")]
+        public int Id { get; set; }
+
+        [Display(Name = "Cliente")]
+        public int IdCliente { get; set; }
+
+        [Display(Name = "Fecha del Pedido")]
+        public DateTime Fecha { get; set; }
+
+        [Display(Name = "Estado del Pedido")]
+        public string Estado { get; set; }
+
+        [Display(Name = "Total del Pedido")]
+        public decimal Total { get; set; }
+    }
+}
